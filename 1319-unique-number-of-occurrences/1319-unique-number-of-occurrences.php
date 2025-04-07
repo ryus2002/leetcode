6e1a6bdc734a1ba6a6210947d8cc165b4335b5ee
ryus2002 <ryus2002@gmail.com>
2025-04-07 17:05:16 +0800

    Time: 0 ms (100%), Space: 20.5 MB (73.68%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.php
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.php
@@ -11,21 +11,40 @@ class Solution {
 
         $size = count($arr);
         $temp_array = [];
-        for ($i = 0; $i <= $size - 1; $i++) {
-            // $value = $arr[$i];
-            $temp_array[$arr[$i]]['count']++;
-        }
+        // for ($i = 0; $i <= $size - 1; $i++) {
+        //     // $value = $arr[$i];
+        //     $temp_array[$arr[$i]]['count']++;
+        // }
 
-        $count = [];
-        foreach ($temp_array as $index => $value) {
-            if ( isset( $count[$temp_array[$index]['count']] ) ) {
+        // 計算頻率
+        foreach ($arr as $num) {
+            if (!isset($temp_array[$num])) {
+                $temp_array[$num] = 0;
+            }
+            $temp_array[$num]++;
+        }        
+
+        // $count = [];
+        // foreach ($temp_array as $index => $value) {
+        //     if ( isset( $count[$temp_array[$value]] ) ) {
+        //         return false;
+        //     }
+        //     else {
+        //         $count[$temp_array[$value]] = true;
+        //     }
+
+        // }
+
+
+        // 檢查頻率的唯一性
+        $occurrences = [];
+        foreach ($temp_array as $count) {
+            if (isset($occurrences[$count])) {
                 return false;
             }
-            else {
-                $count[$temp_array[$index]['count']] ++;
-            }
-
+            $occurrences[$count] = true;
         }
+        
         return true;
     }
 }
