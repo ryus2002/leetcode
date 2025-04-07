f5835bf05f2c639a1598f9eeece2309c9b6bb640
ryus2002 <ryus2002@gmail.com>
2025-04-07 15:27:24 +0800

    Time: 16 ms (44.57%), Space: 34.3 MB (5.71%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.php
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.php
@@ -10,25 +10,23 @@ class Solution {
             return "";
         }
 
-        $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+        $str_array = ['a','e','i','o','u','A','E','I','O','U'];
         $temp_array = [];
 
-        $key = 0;
+       $key = 0;
         for ($i = 0; $i < strlen($s); $i++) {
-            if (in_array($s[$i], $vowels)) {
+            if ( in_array($s[$i], $str_array) ) {
                 $temp_array[$key]['key'] = $i;
                 $temp_array[$key]['value'] = $s[$i];
                 $key++;
             }
         }
 
-        $result = str_split($s);
-        $total = count($temp_array);
-        
-        for ($i = 0; $i < $total; $i++) {
-            $result[$temp_array[$i]['key']] = $temp_array[$total - 1 - $i]['value'];
+        $keyindex = count($temp_array);
+        foreach ($temp_array as $index => $value) {
+            $s[$temp_array[$index]['key']] = $temp_array[$keyindex-1]['value'];
+            $keyindex--;
         }
-        
-        return implode('', $result);
+        return $s;
     }
 }
