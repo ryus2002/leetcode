872099b48fa17a29686df268d048d9d4601f1c2d
ryus2002 <ryus2002@gmail.com>
2025-04-07 16:55:59 +0800

    Time: 1 ms (12.28%), Space: 21 MB (5.26%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.php
@@ -0,0 +1,32 @@
+class Solution {
+
+    /**
+     * @param Integer[] $arr
+     * @return Boolean
+     */
+    function uniqueOccurrences($arr) {
+        if (count($arr) <= 1) {
+            return false;
+        }
+
+        $size = count($arr);
+        $temp_array = [];
+        for ($i = 0; $i <= $size - 1; $i++) {
+            $value = $arr[$i];
+            $temp_array[$value]['count']++;
+        }
+
+        $count = [];
+        foreach ($temp_array as $index => $value) {
+// echo $temp_array[$index]['count'];exit;
+            if ( isset( $count[$temp_array[$index]['count']] ) ) {
+                return false;
+            }
+            else {
+                $count[$temp_array[$index]['count']] ++;
+            }
+
+        }
+        return true;
+    }
+}
